ec563f10d91fea8e4a5f103219b8271b644e877c
nverba <distantstate@gmail.com>
2015-02-18 10:29:56 +0000

    added jshint JSON config

--- pikaday-angular.js
+++ pikaday-angular.js
@@ -1,65 +1,70 @@
-'use strict';
+(function () {
 
-angular.module('angularPikaday', [])
-  .provider('pikaday', function() {
-    var config = {};
+  'use strict';
 
-    this.$get = function() {
-      return config;
-    };
+  angular.module('angularPikaday', [])
+    .provider('pikaday', function() {
 
-    this.setConfig = function setConfig(pikadayConfig) {
-      config = pikadayConfig;
-    };
+      var config = {};
 
-  })
-  .directive('pikaday', ['pikaday', function(pikaday) {
-  return {
-    restrict: 'A',
-    scope: {
-      pikaday: '=',
-      onSelect: '&'
-    },
-    link: function (scope, elem, attrs) {
+      this.$get = function() {
+        return config;
+      };
 
-      var picker = new Pikaday({
+      this.setConfig = function setConfig(pikadayConfig) {
+        config = pikadayConfig;
+      };
 
-        field: elem[0],
-        trigger: document.getElementById(attrs.triggerId),
-        bound: attrs.bound !== 'false',
-        position: attrs.position || '',
-        format: attrs.format || 'ddd MMM D YYYY', // Requires Moment.js for custom formatting
-        defaultDate: new Date(attrs.defaultDate),
-        setDefaultDate: attrs.setDefaultDate === 'true',
-        firstDay: attrs.firstDay ? parseInt(attrs.firstDay) : 0,
-        minDate: new Date(attrs.minDate),
-        maxDate: new Date(attrs.maxDate),
-        yearRange: attrs.yearRange ? JSON.parse(attrs.yearRange) : 10, // Accepts int (10) or 2 elem array ([1992, 1998]) as strings
-        isRTL: attrs.isRTL === 'true',
-        i18n: pikaday.i18n || {
-          previousMonth : 'Previous Month',
-          nextMonth     : 'Next Month',
-          months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
-          weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-          weekdaysShort : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
-        },
-        yearSuffix: attrs.yearSuffix || '',
-        showMonthAfterYear: attrs.showMonthAfterYear === 'true',
+    })
+    .directive('pikaday', ['pikaday', function(pikaday) {
+    return {
+      restrict: 'A',
+      scope: {
+        pikaday: '=',
+        onSelect: '&'
+      },
+      link: function (scope, elem, attrs) {
 
-        onSelect: function () {
-          if ('onSelect' in attrs) {
-            scope.onSelect({ pikaday: this });
+        var picker = new Pikaday({
+
+          field: elem[0],
+          trigger: document.getElementById(attrs.triggerId),
+          bound: attrs.bound !== 'false',
+          position: attrs.position || '',
+          format: attrs.format || 'ddd MMM D YYYY', // Requires Moment.js for custom formatting
+          defaultDate: new Date(attrs.defaultDate),
+          setDefaultDate: attrs.setDefaultDate === 'true',
+          firstDay: attrs.firstDay ? parseInt(attrs.firstDay) : 0,
+          minDate: new Date(attrs.minDate),
+          maxDate: new Date(attrs.maxDate),
+          yearRange: attrs.yearRange ? JSON.parse(attrs.yearRange) : 10, // Accepts int (10) or 2 elem array ([1992, 1998]) as strings
+          isRTL: attrs.isRTL === 'true',
+          i18n: pikaday.i18n || {
+            previousMonth : 'Previous Month',
+            nextMonth     : 'Next Month',
+            months        : ['January','February','March','April','May','June','July','August','September','October','November','December'],
+            weekdays      : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+            weekdaysShort : ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+          },
+          yearSuffix: attrs.yearSuffix || '',
+          showMonthAfterYear: attrs.showMonthAfterYear === 'true',
+
+          onSelect: function () {
+            if ('onSelect' in attrs) {
+              scope.onSelect({ pikaday: this });
+            }
+            setTimeout(function(){
+              scope.$apply();
+            });
           }
-          setTimeout(function(){
-            scope.$apply();
-          });
-        }
-      });
-      scope.pikaday = picker;
+        });
+        scope.pikaday = picker;
 
-      scope.$on('$destroy', function () {
-        picker.destroy();
-      });
-    }
-  };
-}]);
+        scope.$on('$destroy', function () {
+          picker.destroy();
+        });
+      }
+    };
+  }]);
+
+})();
