27b03f0c49405e3b16e81c120d150f3dab087e8a
nverba <distantstate@gmail.com>
2015-03-27 15:04:28 +0000

    seperated config & inline attribute tests

--- test/pikaday_test.js
+++ test/pikaday_test.js
@@ -1,22 +1,73 @@
 describe('directive: pikaday', function() {
-  var element, scope;
 
   beforeEach(module('pikaday'));
+  beforeEach(module(function(pikadayConfigProvider) {}));
 
-  beforeEach(inject(function($rootScope, $compile) {
-    scope = $rootScope.$new();
+  // INLINE
 
-    element = '<input pikaday="myPickerObject" number-of-months="2" >';
+  describe('inline', function() {
 
-    element = $compile(element)(scope);
-    scope.$digest();
+    var element, scope, provider;
 
-  }));
+    beforeEach(inject(function($rootScope, $compile) {
 
-  describe('inline attribute', function() {
+      scope    = $rootScope.$new();
+      element  = '<input pikaday="myPickerObject" number-of-months="2" >';
+      compiled = $compile(element)(scope);
 
-    it("number-of-months should be applied", function() {
-      assert.equal(scope.myPickerObject._o.numberOfMonths, 2);
+      scope.$digest();
+
+    }));
+
+    describe('attribute', function() {
+
+      it("number-of-months should be applied", function() {
+        assert.equal(scope.myPickerObject._o.numberOfMonths, 2);
+      });
     });
+
+  });
+
+ // PROVIDER
+
+  describe('provider', function() {
+
+    var element, scope;
+
+    var config = {
+
+      locales: {
+
+        de: {
+          previousMonth : 'Vorheriger Monat',
+          nextMonth     : 'Nächster Monat',
+          months        : ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+          weekdays      : ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+          weekdaysShort : ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."]
+        }
+      },
+
+      disableWeekends: true
+    };
+
+    beforeEach(inject(function($rootScope, $compile, pikadayConfig) {
+
+      angular.extend(pikadayConfig, config);
+
+      scope    = $rootScope.$new();
+      element  = '<input pikaday="myPickerObject">';
+      compiled = $compile(element)(scope);
+
+      scope.$digest();
+
+    }));
+
+    describe('config', function() {
+
+      it("should disable weekends", function() {
+        assert.equal(scope.myPickerObject._o.disableWeekends, true);
+      });
+    });
+
   });
 });
