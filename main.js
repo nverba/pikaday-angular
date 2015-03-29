797c567bd970371ab8d7c89c32b0c91f283595e7
nverba <distantstate@gmail.com>
2015-03-29 14:01:24 +0100

    added main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+require('angular');
+
+// angular-pikaday binds to the main angular object,
+// so does not need to be allocated to a variable.
+
+require('pikaday-angular');
+
+angular.module('exampleApp', ['pikaday'])
+  .config(['pikadayConfigProvider', function(pikaday) {
+    var locales = {
+      de: {
+        previousMonth : 'Vorheriger Monat',
+        nextMonth     : 'Nächster Monat',
+        months        : ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+        weekdays      : ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+        weekdaysShort : ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."]
+      }
+    };
+    pikaday.setConfig({
+      locales: locales
+      // disableDayFn: function (day) {
+      //   console.log(arguments);
+      // }
+    });
+  }])
+  .controller('exampleController', function() {
+
+    this.local = "de";
+    this.onPikadaySelect = function onPikadaySelect(pikaday) {
+      alert(pikaday.toString());
+    };
+    this.filterDays = function filterDaysFn(date) {
+      return date.getDay() === 3;
+    };
+  });
