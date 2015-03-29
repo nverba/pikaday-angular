79d8dbb035c6ce77043dfdb4eb887aba3947ab8e
nverba <distantstate@gmail.com>
2015-03-29 16:17:15 +0100

    added gulp-markdown & inject recipe

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp       = require('gulp');
 var transform  = require('vinyl-transform');
 var uglify     = require('gulp-uglify');
 var concat     = require('gulp-concat');
+var markdown   = require('gulp-markdown');
+var inject     = require('gulp-inject');
 
 var paths = {
   css: ["node_modules/pikaday-angular/node_modules/pikaday/css/pikaday.css"]
@@ -27,4 +29,24 @@ gulp.task('css', function () {
   return gulp.src(paths.css)
     .pipe(concat('all.css'))
     .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('markdown', function () {
+
+  return gulp.src("src/*.md")
+    .pipe(markdown())
+    .pipe(gulp.dest('./tmp'));
+  });
+
+gulp.task('index', ['markdown'], function () {
+
+  var target  = gulp.src('./index.html');
+  var sources = gulp.src('./tmp/*.html');
+
+  return target.pipe(inject(sources, {
+    transform: function (filePath, file) {
+      return file.contents.toString('utf8');
+    }
+  }))
+  .pipe(gulp.dest('./'));
 });
